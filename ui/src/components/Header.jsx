e998bfd6c6c33d48494cc805b99314b106dc575b
GabrielIkpolo <contact.gikps@gmail.com>
2025-05-11 12:18:15 -0400

    Designed the header

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import "./header.css";
+import React from 'react';
+import "./header.css"; 
+import logo from '../assets/images/logo-1-300x69.png'; 
 
 const Header = () => {
-  return (
-    <div className='header'>Header</div>
-  )
-}
 
-export default Header
+  
+  return (
+    <header className="header">
+      <div className="header-container">
+        {/* Logo */}
+        <div className="logo">
+          <img src={logo} alt="Adeleke University Logo" />
+        </div>
+
+        {/* Title */}
+        <h1 className="title">Lost and Found Items</h1>
+
+        {/* Navigation Links */}
+        <nav className="nav-links">
+          <a href="#" className="nav-link">Login</a>
+          <span className="separator">|</span>
+          <a href="#" className="nav-link">Logout</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
